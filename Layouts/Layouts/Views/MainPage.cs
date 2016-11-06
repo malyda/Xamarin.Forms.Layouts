bf387f349feb1b2073261848ea2a68fa58fa6e8c
Bluf <Bluf@DESKTOP-RHAB0QV>
2016-11-06 12:56:43 +0100

    Add project files.

---
+++ Layouts/Layouts/Views/MainPage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Layouts.Views
+{
+    public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Choose one of pages"
+                        },
+                        new Button
+                        {
+                            Text = "StackLayout",
+                             Command = showPageCommand(new StackLayoutPage())
+                        },
+                        new Button
+                        {
+                            Text = "AbsoluteLayout",
+                            Command = showPageCommand(new AbsolutePage())
+                        },
+                        new Button
+                        {
+                            Text = "RelativeLayout",
+                             Command = showPageCommand(new RelativeLayoutPage())
+                        }
+                    }
+            };
+    }
+
+        Command showPageCommand(Page page)
+        {
+           return new Command((() =>
+           {
+               Navigation.PushAsync(page);
+           }));
+                
+               
+        }
+    }
+}
